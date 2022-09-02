06bd18d6f7e02d0fbaed2482c85581abeac9fa4f
AngieMwaniki <angela.mwaniki@student.moringaschool.com>
2022-09-02 16:10:09 +0300

    commit

---
+++ index.js
@@ -0,0 +1,8 @@
+/*element.addEventListener("click", function() {
+    document.getElementById("one"). alert= "Welcome, Thanks for subscribing. you are in for a treat!!";
+  });*/
+  const element = document.querySelector("#one");
+
+element.addEventListener("click", () => {
+	console.log("Welcome, Thanks for subscribing. you are in for a treat!!");
+});
