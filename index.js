1b355f018a2d3643c8b9b4ceb7ed7bdb6c280bf5
AngieMwaniki <angela.mwaniki@student.moringaschool.com>
2022-09-05 00:24:09 +0300

    deleted some images

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 alert('Welcome to FitPro, please subscribe and leave a comment.')
-let submit = document.querySelector ("#one")
+
+const submit = document.querySelector ("#one")
 submit.addEventListener('click', ()=>{
   alert('Thanks for signing up to FitPro gym. YOU ARE IN FOR A RIDE!!')
 })
 
+const likeCount = document.querySelector('#text');
+const dislikeCount = document.querySelector('#dislike');
+
+function count(){likeCount.value = parseInt(likeCount.value) ++1;
+}
+
+function count(){dislikeCount.value = parseInt(dislikecount.value) ++1;
+} 
 
 var post= document.getElementById("post");
 post.addEventListener("click", function(){
     var commentBoxValue= document.getElementById("comment-box").value;
- 
+    
     var li = document.createElement("li");
     var text = document.createTextNode(commentBoxValue);
     li.appendChild(text);
     document.getElementById("unordered").appendChild(li);
- 
+    
 });
