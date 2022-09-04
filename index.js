69331bba00d452bb34787a725651dee4298afb0c
AngieMwaniki <angela.mwaniki@student.moringaschool.com>
2022-09-04 20:49:28 +0300

    worked on js

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ let submit = document.querySelector ("#one")
 submit.addEventListener('click', ()=>{
   alert('Thanks for signing up to FitPro gym. YOU ARE IN FOR A RIDE!!')
 })
+
+
+var post= document.getElementById("post");
+post.addEventListener("click", function(){
+    var commentBoxValue= document.getElementById("comment-box").value;
+ 
+    var li = document.createElement("li");
+    var text = document.createTextNode(commentBoxValue);
+    li.appendChild(text);
+    document.getElementById("unordered").appendChild(li);
+ 
+});
